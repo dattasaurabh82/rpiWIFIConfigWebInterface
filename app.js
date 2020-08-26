8f0fab019c8d66f1595a76d7bb37ea32d2abe563
Saurabh Datta <hi@dattasaurabh.com>
2020-08-27 01:33:37 +0800

    added a wifi scanner and object exporter

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 var express = require('express');
-var fs = require('fs-extra');
+var wifi = require("node-wifi");
+const bodyParser = require('body-parser');
 
 var HTTP_PORT = 4000;
 
 var app = express();
 var server = app.listen(HTTP_PORT);
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+// Initialize wifi module
+// Absolutely necessary even to set interface to null
+wifi.init({
+  iface: null // network interface, choose a random wifi interface if set to null
+});
+
+
+
+app.post('/scan', function(req, res) {
+    console.log("browser has loaded, now scanning networks\n");
+
+    // Scan networks
+    wifi.scan((error, networks) => {
+	  if (error) {
+	    console.log(error);
+	  } else {
+	  	ssids = [];
+	    for(var id=0; id<networks.length; id++){
+	    	ssids.push(String(networks[id].ssid));
+	    }
+	    console.log(ssids);
+
+	    // send the data to browser
+	    data = {"ssids": ssids};
+    	res.status(200).send(ssids);
+	  }
+	});
+});
 
 
 if (server) {
   console.log("\nhttp  server running at port: " + HTTP_PORT);
 }
+
+
--- public/script.js
+++ public/script.js
@@ -1,21 +1,82 @@
-console.log('loaded from file');
+// console.log('loaded from file');
 
 const WIFI_PARAMS = {
-  ssid: '',
-  psk: 'WPA/WPA2 PSK'
+  WIFI_SSID: '',
+  WIFI_PSWD: 'WPA/WPA2 PSK'
 };
 
-const pane = new Tweakpane();
+const pane = new Tweakpane({
+  title: 'CONFIGURE WIFI',
+});
 
-// ssid: ssid
 
-pane.addInput(WIFI_PARAMS, 'ssid', {
-	options: {
-		dark: 'path/to/dark.json',
-		light: 'path/to/Light.json'
+
+// pane.addInput(WIFI_PARAMS, 'WIFI_SSID', {
+// 	options: {
+// 		scanning: ssidlist,
+// 	}
+// });
+
+
+// pane.addInput(WIFI_PARAMS, 'WIFI_PSWD').on('change', (value) => {
+// 	console.log('wifi psk: ' + String(value));
+// 	pane.refresh();	
+// });
+
+
+function fillSSID(SSIDARRAY){
+
+	ssidlist = {};
+
+	wifiCreds = {
+		'ssid': '',
+		'psk': ''
+	};
+	
+	// generating self key value pair of ssids from returned ssid array into an object
+	for (id=0; id< SSIDARRAY.length; id++){
+		ssidlist[String(SSIDARRAY[id])] = String(SSIDARRAY[id]);
 	}
-});
 
-pane.addInput(WIFI_PARAMS, 'psk').on('change', (value) => {
-	console.log('wifi psk: ' + String(value));
-});
+	console.log(ssidlist);
+
+	// populate the pane with list of SSID
+	pane.addInput(WIFI_PARAMS, 'WIFI_SSID', {
+		options: ssidlist
+	}).on('change', (ssid) => {
+		console.log('wifi ssid: ' + String(ssid));
+		wifiCreds.ssid = String(ssid);
+	}); 
+
+	// create a fild for wifi password
+	pane.addInput(WIFI_PARAMS, 'WIFI_PSWD').on('change', (psk) => {
+		console.log('wifi psk: ' + String(psk));
+		wifiCreds.psk = String(psk);
+	});
+	
+
+	pane.addButton({title: 'CONNECT'}).on('click', () => {
+		// send the wifi data
+		if (wifiCreds.ssid.length != 0  && wifiCreds.psk.length != 0){
+			console.log(wifiCreds);
+		}
+  	});
+}
+
+
+window.addEventListener('load', function() {
+    console.log('Server is scanning wifi networks..\n');
+    
+    $.ajax({
+        type: "POST",
+        url: "/scan",
+        data: "clean",
+        success: function(ssidList){
+        	console.log('WIFI SSIDS available:\n');
+        	fillSSID(ssidList);
+        },
+        error: function(err){
+        	console.log(err);
+        }
+    });
+})
