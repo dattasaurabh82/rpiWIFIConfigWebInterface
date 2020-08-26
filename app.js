8f2de0fbe43fe5df4d1ff81654dd2dc64317445d
Saurabh Datta <hi@dattasaurabh.com>
2020-08-26 21:14:08 +0800

    added https related ssl certs

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var https = require('https');
 
 var port = 5000;
 var PORT = process.env.PORT || 4000;
@@ -8,3 +9,9 @@ var app = express();
 var server = app.listen(port);
 
 app.use(express.static('public'));
+
+if (server) {
+  console.log(" ");
+  console.log("----------------------------------");
+  console.log("http  server running at port: " + port);
+}
