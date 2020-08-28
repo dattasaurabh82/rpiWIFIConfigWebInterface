c0f338de324a97283613a3fc60273b7337b8091c
Saurabh Datta <hi@dattasaurabh.com>
2020-08-29 03:46:35 +0800

    for parsing wifi ssids cutom script

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
-var wifi = require("node-wifi");
+// var wifi = require("node-wifi");
 const bodyParser = require('body-parser');
+const fs = require('fs-extra')
+var shell = require('shelljs');
 
 var HTTP_PORT = 4000;
 
@@ -14,32 +16,45 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // Initialize wifi module
-wifi.init({
-  iface: null // network interface, choose a random wifi interface if set to null
-});
+// wifi.init({
+//   iface: null // network interface, choose a random wifi interface if set to null
+// });
 
+
+
+// var child = exec('wifi scanner code', {async:true});
+// child.stdout.on('data', function(data) {
+//   /* ... do something with data ... */
+// });
 
 
 app.post('/scan', function(req, res) {
     console.log("browser has loaded, now scanning networks\n");
 
     // Scan networks
-    wifi.scan((error, networks) => {
-	  if (error) {
-	    console.log(error);
-	  } else {
-	  	ssids = [];
-	    for(var id=0; id<networks.length; id++){
-	    	ssids.push(String(networks[id].ssid));
-	    }
-	    console.log('network scanned: \n');
-	    console.log(ssids);
-
-	    // send the data to browser
-	    data = {"ssids": ssids};
-    	res.status(200).send(ssids);
-	  }
+    // scan the wifi and save the data in a file
+	shell.exec('sudo iwlist wlan0 scan | grep ESSID >> .rawssid', function(code, stdout, stderr) {
+		console.log('Exit code:', code);
+		console.log('Program output:', stdout);
+		console.log('Program stderr:', stderr);
 	});
+
+ 	// wifi.scan((error, networks) => {
+	//   if (error) {
+	//     console.log(error);
+	//   } else {
+	//   	ssids = [];
+	//     for(var id=0; id<networks.length; id++){
+	//     	ssids.push(String(networks[id].ssid));
+	//     }
+	//     console.log('network scanned: \n');
+	//     console.log(ssids);
+
+	//     // send the data to browser
+	//     data = {"ssids": ssids};
+ //    	res.status(200).send(ssids);
+	//   }
+	// });
 });
 
 
